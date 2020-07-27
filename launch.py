3d6b22f0215f6e4dd4df2254335b5e9c5082c5db
Sean <stayfrosty00@yahoo.com>
2020-07-27 15:26:37 +0100

    rearanged text to make better use of space

--- launch.py
+++ launch.py
@@ -24,16 +24,19 @@ inky_display.set_border(inky_display.BLACK)
 res = requests.get("https://spaceflightnow.com/launch-schedule/")
 if res.status_code == 200:
 	soup = BeautifulSoup(res.content, "lxml")
-	mission = (soup.find("span", "mission").text)
+	mission_rocket = (soup.find("span", "mission").text)
 	launchdate = (soup.find("span", "launchdate").text)
 	launchtimetemp = soup.find("div", "missiondata").get_text()
 	timeonly = launchtimetemp.split("(")[1].split("EDT")[0]
 	launchsite = launchtimetemp.split("site: ")[1]
+	mission = mission_rocket.split("• ")[1]
+	rocket = mission_rocket.split(" •")[0]
 else:
 	exit("website did not load")
 
 #These print states are used for debugging
 #print(mission)
+#print(rocket)
 #print(launchdate)
 #print(timeonly)
 #print(launchsite)
@@ -45,8 +48,8 @@ font = ImageFont.truetype("resources/Amble-Regular.ttf", fontsize)
 img = Image.open("resources/launchbackground.png")
 draw = ImageDraw.Draw(img)
 
-draw.text((2, 2), "Next Launch", inky_display.WHITE, font)
-draw.text((2, 25), mission, inky_display.BLACK, font)
+draw.text((2, 2), "Next Launch: " + rocket, inky_display.WHITE, font)
+draw.text((2, 26), mission, inky_display.BLACK, font)
 draw.text((2, 52), launchdate + " @ " + timeonly, inky_display.BLACK, font)
 draw.text((2, 78), launchsite, inky_display.BLACK, font)
 
