22b99d24863becebc83d8dc77ff9919847b0dc77
Sean <stayfrosty00@yahoo.com>
2020-07-31 13:09:31 +0100

    Functionized the creation and display of the image. Cycles through first five launches.  Should be ready adding user inputs

--- launch.py
+++ launch.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 from inky import InkyPHAT
 from PIL import Image, ImageDraw, ImageFont
@@ -21,6 +22,9 @@ color = args.color
 inky_display = InkyPHAT(color)
 inky_display.set_border(inky_display.BLACK)
 
+fontsize = 16
+font = ImageFont.truetype("resources/Amble-Regular.ttf", fontsize)
+
 #Scrapes SpaceFlightNow.com's launch schedule. (Thank you)
 res = requests.get("https://spaceflightnow.com/launch-schedule/")
 if res.status_code == 200:
@@ -29,30 +33,38 @@ if res.status_code == 200:
 else:
 	exit("website did not load")
 
+#Prints the raw code from website
+#print(soup.prettify())
+
 def get_data(data,i):
 	"""
-	data=the typenof data you want. i.e. launchdate
-	i=which launch
+	data=the type of data you want. (.launchdate, .mission, .missiondata)
+	i=launch number
 	"""
 	output = soup.select(data)
+	#try:
+	#	output.replace("TBD","(TBDEDTL")
 	return(output[i].text)
 
+def draw_data(i):
+	"""
+	creates the image that will later be sent to the display
+	i=launch number
+	"""
+	draw.text((2, 2), "Next Launch: " + get_data(".mission",i).split(" •")[0], inky_display.WHITE, font)
+	draw.text((2, 26), get_data(".mission",i).split("• ")[1], inky_display.BLACK, font)
+	try:
+		draw.text((2, 52), get_data(".launchdate",i) + " @ " + get_data(".missiondata",i).split("(")[1].split("EDT")[0], inky_display.BLACK, font)
+	except IndexError:
+		draw.text((2, 52), get_data(".launchdate",i) + " @ " + "TBD", inky_display.BLACK, font)
+	draw.text((2, 78), get_data(".missiondata",i).split("site: ")[1], inky_display.BLACK, font)
 
-#Loads the background and prints the text on top
-fontsize = 16
-font = ImageFont.truetype("resources/Amble-Regular.ttf", fontsize)
-
-img = Image.open("resources/launchbackground.png")
-draw = ImageDraw.Draw(img)
-
-
-
-draw.text((2, 2), "Next Launch: " + get_data(".mission",0).split(" •")[0], inky_display.WHITE, font)
-draw.text((2, 26), get_data(".mission",0).split("• ")[1], inky_display.BLACK, font)
-draw.text((2, 52), get_data(".launchdate",0) + " @ " + get_data(".missiondata",0).split("(")[1].split("EDT")[0], inky_display.BLACK, font)
-draw.text((2, 78), get_data(".missiondata",0).split("site: ")[1], inky_display.BLACK, font)
-
-#Sends the image to the inky pHat
-print("Sending image to display")
-inky_display.set_image(img)
-inky_display.show()
+for b in range(5):
+	"""Calls on functions to get data and draw image, then sends image to the Inky pHat"""
+	img = Image.open("resources/launchbackground.png")
+	draw = ImageDraw.Draw(img)
+	draw_data(b)
+	print("Sending image " + str(b+1) + " to display")
+	inky_display.set_image(img)
+	inky_display.show()
+	#time.sleep(5)
