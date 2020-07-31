4f6d7c8ad1453b3b500d7633419d004cdd3962c0
Sean <stayfrosty00@yahoo.com>
2020-07-31 03:22:11 +0100

    functionized the scrapping.  next- functionize the drawing of the data to displace multiple launches

--- launch.py
+++ launch.py
@@ -22,42 +22,21 @@ inky_display = InkyPHAT(color)
 inky_display.set_border(inky_display.BLACK)
 
 #Scrapes SpaceFlightNow.com's launch schedule. (Thank you)
-mission_data=()
+res = requests.get("https://spaceflightnow.com/launch-schedule/")
+if res.status_code == 200:
+	soup = BeautifulSoup(res.content, "lxml")
+	print("Website loaded")
+else:
+	exit("website did not load")
 
-def getdata():
-	res = requests.get("https://spaceflightnow.com/launch-schedule/")
-	if res.status_code == 200:
-		soup = BeautifulSoup(res.content, "lxml")
-		mission_rocket = (soup.find("span", "mission").text)
-		launchdate = (soup.find("span", "launchdate").text)
-		launchtimetemp = soup.find("div", "missiondata").get_text()
-		timeonly = launchtimetemp.split("(")[1].split("EDT")[0]
-		launchsite = launchtimetemp.split("site: ")[1]
-		mission = mission_rocket.split("• ")[1]
-		rocket = mission_rocket.split(" •")[0]
-		
-		#testing trying to get additional launch data
-		#print(soup.prettify())
-		links = soup.select(".mission")
-		for link in links:
-			names = link.contents[0]
-			print(names)
-		
-		return(rocket, mission, launchdate, timeonly, launchsite)
-	else:
-		exit("website did not load")
+def get_data(data,i):
+	"""
+	data=the typenof data you want. i.e. launchdate
+	i=which launch
+	"""
+	output = soup.select(data)
+	return(output[i].text)
 
-mission_data=getdata()
-
-
-
-print(mission_data)
-#These print states are used for debugging
-#print(mission)
-#print(rocket)
-#print(launchdate)
-#print(timeonly)
-#print(launchsite)
 
 #Loads the background and prints the text on top
 fontsize = 16
@@ -68,11 +47,12 @@ draw = ImageDraw.Draw(img)
 
 
 
-draw.text((2, 2), "Next Launch: " + mission_data[0], inky_display.WHITE, font)
-draw.text((2, 26), mission_data[1], inky_display.BLACK, font)
-draw.text((2, 52), mission_data[2] + " @ " + mission_data[3], inky_display.BLACK, font)
-draw.text((2, 78), mission_data[4], inky_display.BLACK, font)
+draw.text((2, 2), "Next Launch: " + get_data(".mission",0).split(" •")[0], inky_display.WHITE, font)
+draw.text((2, 26), get_data(".mission",0).split("• ")[1], inky_display.BLACK, font)
+draw.text((2, 52), get_data(".launchdate",0) + " @ " + get_data(".missiondata",0).split("(")[1].split("EDT")[0], inky_display.BLACK, font)
+draw.text((2, 78), get_data(".missiondata",0).split("site: ")[1], inky_display.BLACK, font)
 
 #Sends the image to the inky pHat
-#inky_display.set_image(img)
-#inky_display.show()
+print("Sending image to display")
+inky_display.set_image(img)
+inky_display.show()
